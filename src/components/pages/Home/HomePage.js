922d7623e9c361a1a3fa0984fe6fdd53a46015b9
Rimpy99 <70006pawel@gmail.com>
2022-07-20 09:18:28 +0200

    entrance slider styled

--- src/components/pages/Home/HomePage.js
+++ src/components/pages/Home/HomePage.js
@@ -8,6 +8,25 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import { motion } from "framer-motion";
 
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+
+const activeSlideVariants = {
+    hidden: {
+        opacity: 0,
+        x: 10,
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: .4,
+            duration: .5,
+            type: "ease-in",
+        }
+    }
+}
+
 const HomePage = () => {
 
     const [slideIndex, setSlideIndex] = useState(0);
@@ -23,10 +42,11 @@ const HomePage = () => {
         autoplay: true,
         infinite: true,
         centerMode: true,
-        speed:500,
+        speed:400,
         autoplaySpeed: 4000,
         slidesToShow:1,
         slidesToScroll:1,
+        variableWidth: true,
         beforeChange: (current, next) => {
           console.log(current);
           setSlideIndex(next);
@@ -39,27 +59,33 @@ const HomePage = () => {
                 {
                     ENTRANCE_SLIDER_DATA.map(({background, title}, index) => {
                         return (
-                            <div className="entrance-slide" key={`slide-${index}`}>
+                            <div className="entrance-slide" key={`slide-${index}`} style={{width: "90vw"}}>
                                 <img 
                                     src={require(`./../../../assets/images/home-images/${background}`)} 
                                     alt="film's wallpaper" 
-                                    className="entrance-slide-image"
+                                    className={`entrance-slide-image ${index !== slideIndex && 'entrance-slide-image-not-active'}`}
                                 />
-                                {/* {index === slideIndex &&  */}
+                                {index === slideIndex && 
                                     <motion.img
                                         src={require(`./../../../assets/images/home-images/${title}`)} 
                                         alt="film's title"
                                         className="entrance-slide-title"
-                                        // variants={activeSlideVariants} 
-                                        // initial="hidden" 
-                                        // animate="visible"
+                                        variants={activeSlideVariants} 
+                                        initial="hidden" 
+                                        animate="visible"
                                     />
-                                {/* } */}
+                                }
                             </div>
                         )
                     })
                 }
             </Slider>
+            <button onClick={() => slider.current.slickPrev()} className="slider-btn slider-btn-prev">
+                <div className="slider-btn-prev-content"><IoIosArrowBack color={"white"} size={40}/></div>
+            </button>
+            <button onClick={() => slider.current.slickNext()} className="slider-btn slider-btn-next">
+                <div className="slider-btn-next-content"><IoIosArrowForward color={"white"} size={40}/></div>
+            </button>
         </div>
     )
 }
