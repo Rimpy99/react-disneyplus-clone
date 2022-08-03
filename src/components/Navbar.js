4f9aefe5897754752f32d6cddaba5e84f01562c8
Rimpy99 <70006pawel@gmail.com>
2022-08-03 09:34:48 +0200

    Added sticky positioning to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,141 +53,156 @@ const Navbar = () => {
     const [isProfileOnHover, setIsProfileOnHover] = useState(false);
     const [areDotsOnHover, setAreDotsOnHover] = useState(false);
 
+    const [changeNavBackground, setChangeNavBackground] = useState(false);
+
+    const changeNavBackgroundCondition = () => {
+        if(window.scrollY >= 20){
+            setChangeNavBackground(true);
+        }else{
+            setChangeNavBackground(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeNavBackgroundCondition);
+
     return(
-        <div className="nav-container">
-            <div className="navigation">
-                <Link to="/"><img src={require("./../assets/images/disney-logo.png")} alt="disney logo" className="logo"/></Link>
-                <ul className="nav-list">
-                    <li>
-                        <Link to="/" className="nav-link">
-                            <div className="nav-icon">
-                                <AiFillHome/>
-                            </div>
-                            <p className="nav-page-title nav-page-title-first">HOME</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/search" className="nav-link">
-                            <div className="nav-icon">
-                                <FiSearch/>
-                            </div>
-                            <p className="nav-page-title">SEARCH</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/watchlist" className="nav-link">
-                            <div className="nav-icon">
-                                <BsPlusLg/>
-                            </div>
-                            <p className="nav-page-title">WATCHLIST</p>
-                        </Link>
-                    </li>
-                    <li className="hidden-in-responsive">
-                        <Link to="/originals" className="nav-link">
-                            <div className="nav-icon">
-                                <AiFillStar/>
-                            </div>
-                            <p className="nav-page-title">ORIGINALS</p>
-                        </Link>
-                    </li>
-                    <li className="hidden-in-responsive">
-                        <Link to="/movies" className="nav-link">
-                            <div className="nav-icon">
-                                <RiMovie2Fill/>
-                            </div>
-                            <p className="nav-page-title">MOVIES</p>
-                        </Link>
-                    </li>
-                    <li className="hidden-in-responsive">
-                        <Link to="/series" className="nav-link">
-                            <div className="nav-icon">
-                                <BsDisplayFill/>
-                            </div>
-                            <p className="nav-page-title">SERIES</p>
-                        </Link>
-                    </li>
-                    <li className="hidden-out-of-responsive">
-                        <div 
-                            className="nav-icon"
-                            onMouseOver={()=>setAreDotsOnHover(true)} 
-                            onMouseOut={()=>setAreDotsOnHover(false)}
-                        >
-                            <div className="nav-icon-dots">
-                                <BsThreeDotsVertical color={"white"} size={20}/>
-                            </div>
-                            <AnimatePresence>
-                                {areDotsOnHover && 
-                                    <motion.div 
-                                        className="nav-dropdown"
-                                        variants={dropdownVariants}
-                                        initial="hidden"
-                                        animate="visible"
-                                        exit={{opacity: 0}}
-                                    >
-                                        <span>
-                                            <Link to="/originals" className="nav-dropdown-item">
-                                                <div className="nav-icon">
-                                                    <AiFillStar color={"white"}/>
-                                                </div>
-                                                <p className="nav-page-title-res">ORIGINALS</p>
-                                            </Link>
-                                        </span>
-                                        <span>
-                                            <Link to="/movies" className="nav-dropdown-item">
-                                                <div className="nav-icon">
-                                                    <RiMovie2Fill color={"white"}/>
-                                                </div>
-                                                <p className="nav-page-title-res">MOVIES</p>
-                                            </Link>
-                                        </span>
-                                        <span>
-                                            <Link to="/series" className="nav-dropdown-item">
-                                                <div className="nav-icon">
-                                                    <BsDisplayFill color={"white"}/>
-                                                </div>
-                                                <p className="nav-page-title-res">SERIES</p>
-                                            </Link>
-                                        </span>
-                                    </motion.div>
-                                }
-                            </AnimatePresence>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div 
-                className="profile" 
-                onMouseOver={()=>setIsProfileOnHover(true)} 
-                onMouseOut={()=>setIsProfileOnHover(false)}
-            >
-                <p>Paul</p>
-                <img src={require("./../assets/images/user.jpg")} alt="user" className="user-img"/>
-                {isProfileOnHover && 
-                    <motion.div 
-                        className="settings-container"
-                        variants={settingsContainerVariants}
-                        initial="hidden"
-                        animate="visible"
-                    >
-                        <motion.div
-                            variants={settingsVariants}
-                        >
-                            <div className="add-profile-container settings-option">
-                                <div className="add-profile-btn">
+        <div className={`nav-container ${changeNavBackground ? "nav-container-after-scroll" : "nav-container-initial"}`}>
+            <div className="nav-content">
+                <div className="navigation">
+                    <Link to="/"><img src={require("./../assets/images/disney-logo.png")} alt="disney logo" className="logo"/></Link>
+                    <ul className="nav-list">
+                        <li>
+                            <Link to="/" className="nav-link">
+                                <div className="nav-icon">
+                                    <AiFillHome/>
+                                </div>
+                                <p className="nav-page-title nav-page-title-first">HOME</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/search" className="nav-link">
+                                <div className="nav-icon">
+                                    <FiSearch/>
+                                </div>
+                                <p className="nav-page-title">SEARCH</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/watchlist" className="nav-link">
+                                <div className="nav-icon">
                                     <BsPlusLg/>
                                 </div>
-                                Add profile
+                                <p className="nav-page-title">WATCHLIST</p>
+                            </Link>
+                        </li>
+                        <li className="hidden-in-responsive">
+                            <Link to="/originals" className="nav-link">
+                                <div className="nav-icon">
+                                    <AiFillStar/>
+                                </div>
+                                <p className="nav-page-title">ORIGINALS</p>
+                            </Link>
+                        </li>
+                        <li className="hidden-in-responsive">
+                            <Link to="/movies" className="nav-link">
+                                <div className="nav-icon">
+                                    <RiMovie2Fill/>
+                                </div>
+                                <p className="nav-page-title">MOVIES</p>
+                            </Link>
+                        </li>
+                        <li className="hidden-in-responsive">
+                            <Link to="/series" className="nav-link">
+                                <div className="nav-icon">
+                                    <BsDisplayFill/>
+                                </div>
+                                <p className="nav-page-title">SERIES</p>
+                            </Link>
+                        </li>
+                        <li className="hidden-out-of-responsive">
+                            <div 
+                                className="nav-icon"
+                                onMouseOver={()=>setAreDotsOnHover(true)} 
+                                onMouseOut={()=>setAreDotsOnHover(false)}
+                            >
+                                <div className="nav-icon-dots">
+                                    <BsThreeDotsVertical color={"white"} size={20}/>
+                                </div>
+                                <AnimatePresence>
+                                    {areDotsOnHover && 
+                                        <motion.div 
+                                            className="nav-dropdown"
+                                            variants={dropdownVariants}
+                                            initial="hidden"
+                                            animate="visible"
+                                            exit={{opacity: 0}}
+                                        >
+                                            <span>
+                                                <Link to="/originals" className="nav-dropdown-item">
+                                                    <div className="nav-icon">
+                                                        <AiFillStar color={"white"}/>
+                                                    </div>
+                                                    <p className="nav-page-title-res">ORIGINALS</p>
+                                                </Link>
+                                            </span>
+                                            <span>
+                                                <Link to="/movies" className="nav-dropdown-item">
+                                                    <div className="nav-icon">
+                                                        <RiMovie2Fill color={"white"}/>
+                                                    </div>
+                                                    <p className="nav-page-title-res">MOVIES</p>
+                                                </Link>
+                                            </span>
+                                            <span>
+                                                <Link to="/series" className="nav-dropdown-item">
+                                                    <div className="nav-icon">
+                                                        <BsDisplayFill color={"white"}/>
+                                                    </div>
+                                                    <p className="nav-page-title-res">SERIES</p>
+                                                </Link>
+                                            </span>
+                                        </motion.div>
+                                    }
+                                </AnimatePresence>
                             </div>
-                            <div className="settings-option">Edit profiles</div>
-                            <div className="settings-option">App Settings</div>
-                            <div className="settings-option">Account</div>
-                            <div className="settings-option">Help</div>
-                            <div className="settings-option">Log Out</div>
+                        </li>
+                    </ul>
+                </div>
+                <div 
+                    className="profile" 
+                    onMouseOver={()=>setIsProfileOnHover(true)} 
+                    onMouseOut={()=>setIsProfileOnHover(false)}
+                >
+                    <p>Paul</p>
+                    <img src={require("./../assets/images/user.jpg")} alt="user" className="user-img"/>
+                    {isProfileOnHover && 
+                        <motion.div 
+                            className="settings-container"
+                            variants={settingsContainerVariants}
+                            initial="hidden"
+                            animate="visible"
+                        >
+                            <motion.div
+                                variants={settingsVariants}
+                            >
+                                <div className="add-profile-container settings-option">
+                                    <div className="add-profile-btn">
+                                        <BsPlusLg/>
+                                    </div>
+                                    Add profile
+                                </div>
+                                <div className="settings-option">Edit profiles</div>
+                                <div className="settings-option">App Settings</div>
+                                <div className="settings-option">Account</div>
+                                <div className="settings-option">Help</div>
+                                <div className="settings-option">Log Out</div>
+                            </motion.div>
                         </motion.div>
-                    </motion.div>
-                }
+                    }
+                </div>
             </div>
         </div>
+        
     )
 }
 
