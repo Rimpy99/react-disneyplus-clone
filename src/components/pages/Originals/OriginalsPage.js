ed76caf0a6e0a26b5701db5907d6f6efd6d92061
Rimpy99 <70006pawel@gmail.com>
2022-08-05 08:40:54 +0200

    OriginalsPage component created

--- src/components/pages/Originals/OriginalsPage.js
+++ src/components/pages/Originals/OriginalsPage.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import './OriginalsPage.css';
+
+import MovieSliderRow from "./../Home/HomeComponents/MovieSlidersComponents/MovieSliderRow";
+import requests from "../../../API/Api";
+
 const OriginalsPage = () => {
 
+    const [giveOnScrollClass, setGiveOnScrollClass] = useState(false);
+
+    const giveHeaderOnScrollClass = () => {
+        if(window.scrollY === 0){
+            setGiveOnScrollClass(false);
+        }else{
+            setGiveOnScrollClass(true);
+        }
+    }
+
+    window.addEventListener('scroll', giveHeaderOnScrollClass);
+
     return(
-        <h1>OriginalsPage</h1>
+        <div className="originals-container">
+            <header className={`${giveOnScrollClass && "header-after-scroll"}`}>
+                <h2 className={`originals-headline ${giveOnScrollClass ? "originals-headline-after-scroll" : "originals-headline-initial"}`}>ORIGINALS</h2>
+            </header>
+            <div originals-sliders-container>
+                <MovieSliderRow title={"Top Rated"} fetchUrl={requests.fetchTopRated}/>
+                <MovieSliderRow title={"Action Movies"} fetchUrl={requests.fetchActionMovies}/>
+                <MovieSliderRow title={"Comedies"} fetchUrl={requests.fetchComedyMovies}/>
+                <MovieSliderRow title={"Horrors"} fetchUrl={requests.fetchHorrorMovies}/>
+                <MovieSliderRow title={"Action Movies"} fetchUrl={requests.fetchActionMovies}/>
+            </div>
+        </div>
     )
 }
 
