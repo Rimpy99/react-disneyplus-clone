34876542862662179d2b2d9f4b2cafacb27f2e56
Rimpy99 <70006pawel@gmail.com>
2022-08-09 17:31:50 +0200

    SearchPage created, searching function created

--- src/components/pages/Search/SearchPage.tsx
+++ src/components/pages/Search/SearchPage.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 
+import "./SearchPage.css";
+
 import axios from './../../../API/Axios';
 import requests from "./../../../API/Api";
 
@@ -7,12 +9,15 @@ const baseUrl = "https://image.tmdb.org/t/p/original/";
 const fetchUrl = requests.fetchComedyMovies;
 
 const SearchPage = () => {
-    const [ movies, setMovies ] = useState(0);
+    const [ moviesFromApi, setMoviesFromApi ] = useState<any[]>([]);
+    const [ searchInput, setSearchInput ] = useState<string>('');
+    const [ movies, setMovies ] = useState<any[]>([]);
 
     useEffect(()=>{
         const fetchData = async () => {
             const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
+            console.log(request.data.results);
+            setMoviesFromApi(request.data.results);
         }
 
         try{
@@ -22,8 +27,42 @@ const SearchPage = () => {
         }
     }, [fetchUrl]);
 
+
+    useEffect(()=>{
+        if(searchInput === ''){
+            setMovies(moviesFromApi);
+        }else{
+            setMovies(
+                moviesFromApi.filter((elem)=>{
+                    return elem.title.toLowerCase().includes(searchInput.toLowerCase());
+                })
+            );
+        }
+    },[moviesFromApi, searchInput]);
+
     return(
-        <h1>SearchPage</h1>
+        <div className="search-container">
+            <input 
+                type="text" 
+                className="search-engine" 
+                placeholder="Search by title, character, or genre"
+                onChange={(e)=>setSearchInput(e.target.value)}
+            />
+            <div className="search-content">
+                <div className="search-content-movies">
+                    {movies.map((movie,index)=>{
+                        return( 
+                            <div className="movie-block-container" key={`movie-block-${index}`}>
+                                <div className="movie-block-content">
+                                    <img src={`${baseUrl}${movie.poster_path}`} className='movie-block-img'/>
+                                    <div className="movie-block-border"/>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
 
