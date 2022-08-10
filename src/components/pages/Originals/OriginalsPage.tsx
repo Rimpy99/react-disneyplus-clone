9da46f9261b54a41f042084983d9e3d3eedf5401
Rimpy99 <70006pawel@gmail.com>
2022-08-10 09:16:37 +0200

    OriginalsPage problem with resizing header fixed

--- src/components/pages/Originals/OriginalsPage.tsx
+++ src/components/pages/Originals/OriginalsPage.tsx
@@ -1,29 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './OriginalsPage.css';
 
-import MovieSliderRow from "./../Home/HomeComponents/MovieSlidersComponents/MovieSliderRow";
+import MovieSliderRow from "../Home/HomeComponents/MovieSlidersComponents/MovieSliderRow";
 import requests from "../../../API/Api";
 
 const OriginalsPage = () => {
 
-    const [giveOnScrollClass, setGiveOnScrollClass] = useState(false);
+    const [giveOnScrollClass, setGiveOnScrollClass] = useState<boolean>(false);
+    const [scrollPosition, setScrollPosition] = useState<number>(0);
 
-    const giveHeaderOnScrollClass = () => {
-        if(window.scrollY === 0){
+    useEffect(()=>{
+        if(scrollPosition < 1){
             setGiveOnScrollClass(false);
-        }else{
+        }else if(scrollPosition >= 1){
             setGiveOnScrollClass(true);
         }
-    }
+    },[scrollPosition])
 
-    window.addEventListener('scroll', giveHeaderOnScrollClass);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+    
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return(
         <div className="originals-container">
             <header className={`${giveOnScrollClass && "header-after-scroll"}`}>
                 <h2 className={`originals-headline ${giveOnScrollClass ? "originals-headline-after-scroll" : "originals-headline-initial"}`}>ORIGINALS</h2>
             </header>
-            <div originals-sliders-container>
+            <div className="originals-sliders-container">
                 <MovieSliderRow title={"Top Rated"} fetchUrl={requests.fetchTopRated}/>
                 <MovieSliderRow title={"Action Movies"} fetchUrl={requests.fetchActionMovies}/>
                 <MovieSliderRow title={"Comedies"} fetchUrl={requests.fetchComedyMovies}/>
