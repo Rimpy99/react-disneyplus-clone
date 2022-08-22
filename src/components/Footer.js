319cd9bde3a2ac822461d5095b3138bc18cc8a01
Rimpy99 <70006pawel@gmail.com>
2022-08-22 17:20:20 +0200

    Footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer-content">
+            THIS IS FOOTER
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/pages/Details/Details.tsx
+++ src/components/pages/Details/Details.tsx
@@ -5,9 +5,7 @@ const Details = () => {
         <div className="details-container">
             <div className="details-content">
                 <img src={require("./../../../assets/images/home-images/details/ba-bg.jpg")} className="details-img"/>
-                DETAILS
             </div>
-            <div className="details-space"/>
             <div className="details-footer"/>
         </div>
     )  
